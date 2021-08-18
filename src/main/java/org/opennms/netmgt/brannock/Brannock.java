68f91985939125e96ab9480c25b795eb37ac4e44
Jeff Gehlbach <jeffg@opennms.com>
2021-08-18 11:39:57 -0400

    - Log throwables - Stub in JDBC attributes - Stub in derived values

--- src/main/java/org/opennms/netmgt/brannock/Brannock.java
+++ src/main/java/org/opennms/netmgt/brannock/Brannock.java
@@ -35,9 +35,15 @@ import java.io.IOException;
 import java.io.Writer;
 import java.lang.management.ManagementFactory;
 
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
 import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
+import javax.management.ReflectionException;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -46,25 +52,26 @@ public class Brannock implements BundleActivator {
     private MBeanServer m_mbeanServer;
     private JSONObject m_jsOut;
     private JSONObject m_jsJmxData;
+    private JSONObject m_jsJdbcData;
     
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         m_mbeanServer = ManagementFactory.getPlatformMBeanServer();
         m_jsOut = new JSONObject();
-        
         m_jsJmxData = new JSONObject();
+        m_jsJdbcData = new JSONObject();
         
         m_jsOut.put("brannockVersion", "v1");
+        m_jsOut.put("startupTime", System.currentTimeMillis());
+        m_jsOut.put("brannockErrors", new JSONArray());
 
-        addAttribute("java.lang:type=Runtime", "Name", "StartTime", "Uptime", "VmName", "VmVendor", "VmVersion");
-        addAttribute("java.lang:type=OperatingSystem", "AvailableProcessors", "Name", "OpenFileDescriptorCount", "TotalPhysicalMemorySize", "Version");
-        addAttribute("org.opennms.netmgt.eventd:name=eventlogs.process", "50thPercentile", "75thPercentile", "95thPercentile", "98thPercentile", "99thPercentile", "999thPercentile", "Count", "DurationUnit", "FifteenMinuteRate", "FiveMinuteRate", "Max", "Mean", "MeanRate", "MeanRate", "Min", "OneMinuteRate", "RateUnit", "StdDev");
-        addAttribute("OpenNMS:Name=Queued", "CreatesCompleted", "DequeuedItems", "DequeuedOperations", "ElapsedTime", "EnqueuedOperations", "Errors", "PromotionCount", "SignificantOpsCompleted", "SignificantOpsDequeued", "SignificantOpsEnqueued", "StartTime", "TotalOperationsPending", "UpdatesCompleted");
-        addAttribute("org.opennms.newts:name=repository.samples-inserted", "Count", "FifteenMinuteRate", "FiveMinuteRate", "MeanRate", "OneMinuteRate", "RateUnit");
-        addAttribute("OpenNMS:Name=Pollerd", "ActiveThreads", "CorePoolThreads", "MaxPoolThreads", "NumPolls", "NumPoolThreads", "PeakPoolThreads", "TaskCompletionRatio", "TaskQueuePendingCount", "TaskQueueRemainingCapacity", "TasksCompleted", "TasksTotal");
-        addAttribute("org.opennms.netmgt.flows:name=flowsPersisted", "Count", "FifteenMinuteRate", "FiveMinuteRate", "MeanRate", "OneMinuteRate", "RateUnit");
+        doJmxAttributes();
+        doJdbcAttributes();
+        doDerivedValues();
         
         m_jsOut.put("jmxData", m_jsJmxData);
+        m_jsOut.put("jdbcData", m_jsJdbcData);
+        m_jsOut.put("finishTime", System.currentTimeMillis());
         writeData(m_jsOut.toString(2));
     }
     
@@ -73,18 +80,54 @@ public class Brannock implements BundleActivator {
         
     }
     
-    private void addAttribute(String objectName, String... attributeNames) {
+    private void doJmxAttributes() {
+        addJmxAttribute("java.lang:type=Runtime", "Name", "StartTime", "Uptime", "VmName", "VmVendor", "VmVersion");
+        addJmxAttribute("java.lang:type=OperatingSystem", "AvailableProcessors", "Name", "OpenFileDescriptorCount", "TotalPhysicalMemorySize", "Version");
+        addJmxAttribute("org.opennms.netmgt.eventd:name=eventlogs.process", "50thPercentile", "75thPercentile", "95thPercentile", "98thPercentile", "99thPercentile", "999thPercentile", "Count", "DurationUnit", "FifteenMinuteRate", "FiveMinuteRate", "Max", "Mean", "MeanRate", "MeanRate", "Min", "OneMinuteRate", "RateUnit", "StdDev");
+        addJmxAttribute("OpenNMS:Name=Queued", "CreatesCompleted", "DequeuedItems", "DequeuedOperations", "ElapsedTime", "EnqueuedOperations", "Errors", "PromotionCount", "SignificantOpsCompleted", "SignificantOpsDequeued", "SignificantOpsEnqueued", "StartTime", "TotalOperationsPending", "UpdatesCompleted");
+        addJmxAttribute("org.opennms.newts:name=repository.samples-inserted", "Count", "FifteenMinuteRate", "FiveMinuteRate", "MeanRate", "OneMinuteRate", "RateUnit");
+        addJmxAttribute("OpenNMS:Name=Pollerd", "ActiveThreads", "CorePoolThreads", "MaxPoolThreads", "NumPolls", "NumPoolThreads", "PeakPoolThreads", "TaskCompletionRatio", "TaskQueuePendingCount", "TaskQueueRemainingCapacity", "TasksCompleted", "TasksTotal");
+        addJmxAttribute("org.opennms.netmgt.flows:name=flowsPersisted", "Count", "FifteenMinuteRate", "FiveMinuteRate", "MeanRate", "OneMinuteRate", "RateUnit");        
+    }
+    
+    private void doJdbcAttributes() {
+        addJdbcAttribute("SELECT COUNT(*) AS monitoringLocationCount FROM monitoringlocations", "monitoringLocationCount");
+        addJdbcAttribute("SELECT COUNT(*) AS minionCount FROM monitoringsystems WHERE type = 'Minion'", "minionCount");
+    }
+    
+    private void doDerivedValues() {
+        
+    }
+    
+    private void addJmxAttribute(String objectName, String... attributeNames) {
+        ObjectName objNameActual;
         try {
-            ObjectName objNameActual = new ObjectName(objectName);
-            JSONObject objJSON = new JSONObject();
-            for (String anAttributeName : attributeNames) {
-                Object attrObj = m_mbeanServer.getAttribute(objNameActual, anAttributeName);
-                objJSON.put(anAttributeName, attrObj.toString());
-            }
-            m_jsJmxData.put(objectName, objJSON);
-        } catch (Throwable t) {
-            // gulp
+            objNameActual = new ObjectName(objectName);
+        } catch (MalformedObjectNameException e) {
+            accumulateThrowable(e);
+            return;
         }
+        JSONObject objJSON = new JSONObject();
+        for (String anAttributeName : attributeNames) {
+            Object attrObj;
+            try {
+                attrObj = m_mbeanServer.getAttribute(objNameActual, anAttributeName);
+            } catch (InstanceNotFoundException | AttributeNotFoundException
+                    | ReflectionException | MBeanException e) {
+                accumulateThrowable(e);
+                continue;
+            }
+            objJSON.put(anAttributeName, attrObj.toString());
+        }
+        m_jsJmxData.put(objectName, objJSON);
+    }
+    
+    private void addJdbcAttribute(String query, String... columns) {
+        // TODO actually do the queries
+    }
+    
+    private void accumulateThrowable(Throwable t) {
+        m_jsOut.append("brannockErrors", t);
     }
     
     private void writeData(String data) throws IOException {
